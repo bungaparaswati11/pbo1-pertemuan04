354eb8d97e4310ac7e4cf0f52a06f1e3458e7fb3
bungaparaswati11 <bungaparaswati11@gmail.com>
2020-04-25 13:58:23 -0700

    Tugas Perulangan

---
+++ src/array/tugas2.java
@@ -0,0 +1,12 @@
+package array;
+
+public class tugas2 {
+    public static void main(String[] args) {
+        int n = 20;
+
+        do {
+            System.out.println("Tik " + n);
+            n--;
+        }while(n > 0);
+    }
+}
