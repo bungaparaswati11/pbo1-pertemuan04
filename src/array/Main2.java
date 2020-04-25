f7b42d0f94a52f82473dede5a7424570cd79f611
bungaparaswati11 <bungaparaswati11@gmail.com>
2020-04-24 21:20:02 -0700

    Membuat Multi dimensial Array

--- src/array/Main2.java
+++ src/array/Main2.java
@@ -2,13 +2,36 @@ package array;
 
 public class Main2 {
     public static void main(String[] args) {
-        String mataKuliah[];
-        mataKuliah = new String[10];
+        double[][] nilaiMahasiswa = new double[5][30];
+        double bobot[] = {0.1,0.2,0.3,0.4};
 
-        mataKuliah[0] = "Pemograman Berbasis Objek 1";
-        mataKuliah[1] = "Visual 2";
-        mataKuliah[2] = "Rekayasa Perangkat Lunak";
-        mataKuliah[3] = "Organisasi Komputer";
-        mataKuliah[4] = "Pemograman Web 2";
+        nilaiMahasiswa[0][0] = 80;
+        nilaiMahasiswa[1][0] = 90;
+        nilaiMahasiswa[2][0] = 75;
+        nilaiMahasiswa[3][0] = 70;
+
+        nilaiMahasiswa[4][0] = nilaiMahasiswa[0][0] * bobot [0] +
+                nilaiMahasiswa[1][0] * bobot [1] +
+                nilaiMahasiswa[2][0] * bobot [2] +
+                nilaiMahasiswa[3][0] * bobot [3];
+
+        System.out.println("Nilai si mahasiwa 1 :"+nilaiMahasiswa[4][0]);
+
+        nilaiMahasiswa[0][1] = 80;
+        nilaiMahasiswa[1][1] = 90;
+        nilaiMahasiswa[2][1] = 75;
+        nilaiMahasiswa[3][1] = 70;
+
+        nilaiMahasiswa[4][1] = 0;
+        for (int i=0 ; i<=3 ; i++) {
+            nilaiMahasiswa[4][1] += nilaiMahasiswa[i][1] *bobot[i];
+        }
+        System.out.println("Nilai si mahasiswa 2 :"+nilaiMahasiswa[4][1]);
+
+        double jumlahNilaiAkhir = 0;
+        for (int i=0 ; i<=3 ; i++) {
+            jumlahNilaiAkhir += nilaiMahasiswa[4][i];
+        }
+        System.out.println(jumlahNilaiAkhir);
     }
 }
