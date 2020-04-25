9bfa2b3f79f02bbf4735463aeb0331b267fbd671
bungaparaswati11 <bungaparaswati11@gmail.com>
2020-04-24 20:53:01 -0700

    Membuat For

--- src/perulangan/Main.java
+++ src/perulangan/Main.java
@@ -8,11 +8,10 @@ public class Main {
         System.out.println("Isi Gelas Sekarang : " + isiGelas + "m1");
         System.out.println("Isi Gelas Ketika Penuh : " + penuh + "m1");
 
-        do {
-            isiGelas++;
+        for (isiGelas = 0 ; isiGelas <= penuh ; isiGelas++){
             System.out.println("Sedang mengisi gelas..");
             System.out.println("Isi Gelas Sekarang : " + isiGelas + "m1");
-        }while (isiGelas != penuh);
+        }
 
         System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "m1");
     }
