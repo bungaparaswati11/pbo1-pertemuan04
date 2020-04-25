65f10ef12efc629f9d2abfc0ab3813a92917734f
bungaparaswati11 <bungaparaswati11@gmail.com>
2020-04-24 20:50:37 -0700

    Membuat Do While

--- src/perulangan/Main.java
+++ src/perulangan/Main.java
@@ -8,11 +8,12 @@ public class Main {
         System.out.println("Isi Gelas Sekarang : " + isiGelas + "m1");
         System.out.println("Isi Gelas Ketika Penuh : " + penuh + "m1");
 
-        while(isiGelas != penuh){
+        do {
             isiGelas++;
             System.out.println("Sedang mengisi gelas..");
             System.out.println("Isi Gelas Sekarang : " + isiGelas + "m1");
-        }
+        }while (isiGelas != penuh);
+
         System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "m1");
     }
 }
